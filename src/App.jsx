a00b3d44d0b949308d6c9deb39a3bc76174f953b
4dydd <haidiraditya16@gmail.com>
2024-09-15 17:09:02 +0800

    commit pertama guweh

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./css/index.css";
+
+function App() {
+  return (
+    <>
+      <div className="font-bold">Halo Gais!</div>
+    </>
+  );
+}
+
+export default App;
