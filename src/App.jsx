bda01c713d26b233de8c24e9ca0f006bcb8ade5a
4dydd <haidiraditya16@gmail.com>
2024-09-15 18:41:17 +0800

    mengubah nama webnya jadi Kopi SatSet

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <center className="absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]">
         <div className="font-bold">Halo Gais!</div>
-        <div className="">Selamat Datang di Web Adit!</div>
+        <div className="">Selamat Datang di Web Kopi SatSet (fake-dev)</div>
       </center>
     </>
   );
