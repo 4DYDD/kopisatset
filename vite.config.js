107b863ceec398903d7568d9c404017f57404d06
4dydd <haidiraditya16@gmail.com>
2024-09-15 18:22:47 +0800

    vercel ribet bengat tefak

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kopisatset/",
+  // base: "/kopisatset/",
   server: {
     host: true,
     port: 8007,
